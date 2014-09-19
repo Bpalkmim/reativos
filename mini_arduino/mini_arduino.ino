561f04f11d7c258a4fbb6505f4c71ca1372190a4
Bpalkmim <bpalkmim@gmail.com>
2014-09-19 13:46:28 -0300

    Update and rename arduino/mini-arduino.ino to mini_arduino/mini_arduino.ino
    Mudança do nome para mini_arduino (e da pasta) e alteração da matriz de notas para um vetor.

--- mini_arduino/mini_arduino.ino
+++ mini_arduino/mini_arduino.ino
@@ -1,45 +1,45 @@
-/*
-  Melody
- 
- Plays a melody 
- 
- circuit:
- * 8-ohm speaker on digital pin 8
- 
- created 21 Jan 2010
- modified 30 Aug 2011
- by Tom Igoe 
+// Mini projeto Arduino: Teclado de transposição
+//
+// Bernardo Alkmim
+// Leonardo Kaplan
 
-This example code is in the public domain.
- 
- http://arduino.cc/en/Tutorial/Tone
- 
- */
-  #include "pitches.h"
+#include "pitches.h"
+// Macro para criação da matriz com os valores definidos em pitches.h
 #define not_o(x) NOTE_C##x, NOTE_CS##x, NOTE_D##x, NOTE_DS##x, NOTE_E##x, NOTE_F##x, NOTE_FS##x, NOTE_G##x, NOTE_GS##x, NOTE_A##x, NOTE_AS##x, NOTE_B##x
 #define notes_xxx not_o(1),not_o(2),not_o(3),not_o(4),not_o(5),not_o(6)
-// notes in the melody:
+
+#define NUMBER_OCTAVES 6
+#define NOTES_OCTAVE 12
 
 int tuning = 0;
 
-int notes[6][12] = {notes_xxx};
+int notes[NUMBER_OCTAVES * NOTES_OCTAVE] = {notes_xxx};
 
 int octave_current = 3;
 
 // note durations: 4 = quarter note, 8 = eighth note, etc.:
 int noteDurations[] = {4,4,4 };
 void octave_up(){
-  octave_current = (octave_current+1)%6;
+  octave_current = (octave_current + 1) % NUMBER_OCTAVES;
 }
 void octave_down(){
-  octave_current = (octave_current-1+6)%6;
+  octave_current = (octave_current - 1 + NUMBER_OCTAVES) % NUMBER_OCTAVES;
 }
 void setup() {
   pinMode(2,INPUT_PULLUP);
   pinMode(3,INPUT_PULLUP);
+  //Serial.begin(9600);
 }
 void play(){
-  int melody[] = {*(notes + 12 * octave_current + tuning + 0), *(notes + 12 * octave_current + tuning + 2), *(notes + 12 * octave_current + tuning + 4)};
+   // Deslocamento da nota em função da oitava atual e da afinação
+  int shift = NOTES_OCTAVE * octave_current + tuning;
+  
+  int melody[] = {*(notes + shift + 0), *(notes + shift + 2), *(notes + shift + 4)};
+ 
+  //Serial.println(melody[0]);
+  //Serial.println(melody[1]);
+  //Serial.println(melody[2]);
+  
   for (int thisNote = 0; thisNote < 3; thisNote++) {
 
     // to calculate the note duration, take one second 
